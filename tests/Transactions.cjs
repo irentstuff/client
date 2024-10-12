35926568a2f1eedf36a406a5059ad864581a2cdc
Ying Qi <yingqi96@outlook.com>
2024-10-12 17:36:12 +0800

    transaction_test wip

--- tests/Transactions.cjs
+++ tests/Transactions.cjs
@@ -6,26 +6,12 @@ class ItemsFunction {
     this.driver = driver
   }
 
-  async navigateAddNewItem() {
+  async navigateTestOfferItem(testOfferItemTitle) {
     //navigate
-    await this.driver.get(`${config.baseUrl}/#/AddItem`)
-    // await this.driver.findElement(By.xpath(`//li[@data-menu-id='rc-menu-uuid-58965-4-AddItem']`)).click()
-    await this.driver.wait(until.elementLocated(By.id('addNewItem_title')))
-  }
-
-  async navigateEditItem() {
-    //navigate
-    await this.driver.get(`${config.baseUrl}/#/MyItems`)
-    await this.driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div[2]/div/div[1]/div`)).click()
-    const buttonElement = await this.driver.findElement(By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Edit Item']]"))
-    await this.driver.executeScript('arguments[0].scrollIntoView(true);', buttonElement) // Scroll to the button
-    await this.driver.sleep(500)
-    await buttonElement.click() // Click the button
-  }
-
-  async navigateDeleteItem() {
-    //navigate
-    await this.driver.get(`${config.baseUrl}/#/MyItems`)
+    const searchBar = await this.driver.findElement(By.css('input[placeholder="Search for an item"]'))
+    searchBar.click()
+    searchBar.sendKeys(testOfferItemTitle)
+    // Wait for the element to be located by its visible text
     let firstCard
 
     try {
@@ -34,69 +20,63 @@ class ItemsFunction {
       console.warn(`First path not found: ${error.message}. Trying fallback path.`)
       firstCard = await this.driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div[2]/div/div/div`)).click()
     }
+  }
+
+  async navigateOfferMade() {
+    //navigate
+    await this.driver.get(`${config.baseUrl}/#/OffersMade`)
+    const firstCard = await this.driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div[2]/div/div[1]/div`))
+    if (firstCard === undefined) {
+      firstCard = await this.driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div[2]/div/div/div`))
+    }
+    firstCard.click()
+    const buttonElement = await this.driver.findElement(By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Edit Item']]"))
+    await this.driver.executeScript('arguments[0].scrollIntoView(true);', buttonElement) // Scroll to the button
+    await this.driver.sleep(500)
+    await buttonElement.click() // Click the button
+  }
+
+  async navigateOfferReceived() {
+    //navigate
+    await this.driver.get(`${config.baseUrl}/#/MyItems`)
+    await this.driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div[2]/div/div[1]/div`)).click()
     const buttonElement = await this.driver.findElement(By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Delete Item']]"))
     await this.driver.executeScript('arguments[0].scrollIntoView(true);', buttonElement) // Scroll to the button
     await this.driver.sleep(500)
     await buttonElement.click() // Click the button
   }
 
-  async addNewItem(itemDetails) {
-    //fill form
-    /* ---------------------------------- title --------------------------------- */
-    await this.driver.findElement(By.id('addNewItem_title')).sendKeys(itemDetails.title)
+  async addNewRentalOffer(rentalDetails) {
+    const makeOfferButtonElement = await this.driver.findElement(
+      By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Make Offer']]")
+    )
+    await this.driver.executeScript('arguments[0].scrollIntoView(true);', makeOfferButtonElement) // Scroll to the button
+    await this.driver.sleep(500)
+    await makeOfferButtonElement.click() // Click the button
 
+    //fill form
     /* -------------------------------- category -------------------------------- */
     // Click to open the Select dropdown
-    await this.driver.findElement(By.xpath('//*[@id="addNewItem"]/div[3]/div/div[2]/div/div/div')).click() // Adjust selector based on your app
+    await this.driver.findElement(By.id('makeOffer_rental_dates')).click() // Adjust selector based on your app
 
     // Wait for the dropdown options to appear
-    const catOption = await this.driver.wait(
-      until.elementLocated(By.xpath(`//div[contains(@class, 'ant-select-item-option-content') and text()="${itemDetails.category}"]`)),
-      10000
-    )
-    // Wait until the category option is visible and enabled
-    await this.driver.wait(async () => {
-      const isClickable = (await catOption.isDisplayed()) && (await catOption.isEnabled())
-      return isClickable
-    }, 10000)
-    await catOption.click()
+    const datePicker = await this.driver.wait(until.elementLocated(By.css('.ant-picker-dropdown')), 10000)
+    datePicker.click()
 
-    /* -------------------------------- condition ------------------------------- */
-    // Click to open the Select dropdown
-    await this.driver.findElement(By.xpath('//*[@id="addNewItem"]/div[4]/div/div[2]/div/div/div')).click() // Adjust selector based on your app
-    // Wait for the dropdown options to appear
-    const conditionOption = await this.driver.wait(
-      until.elementLocated(By.xpath(`//div[contains(@class, 'ant-select-item-option-content') and text()="${itemDetails.condition}"]`)),
-      10000
-    )
-    // Wait until the category option is visible and enabled
-    await this.driver.wait(async () => {
-      const isClickable = (await conditionOption.isDisplayed()) && (await conditionOption.isEnabled())
-      return isClickable
-    }, 10000)
-    await conditionOption.click()
+    const startDateToSelect = await this.driver.wait(until.elementLocated(By.xpath(`//td[@title='${rentalDetails.start_date}']`)), 10000)
+    await startDateToSelect.click() // Click on the desired date
 
-    /* -------------------------------- availability ------------------------------- */
-    // Click to open the Select dropdown
-    await this.driver.findElement(By.xpath('//*[@id="addNewItem"]/div[5]/div/div[2]/div/div/div/div')).click() // Adjust selector based on your app
-    // Wait for the dropdown options to appear
-    const availabilityOption = await this.driver.wait(
-      until.elementLocated(By.xpath(`//div[contains(@class, 'ant-select-item-option-content') and text()="${itemDetails.availability}"]`)),
-      10000
-    )
-    // Wait until the category option is visible and enabled
-    await this.driver.wait(async () => {
-      const isClickable = (await availabilityOption.isDisplayed()) && (await availabilityOption.isEnabled())
-      return isClickable
-    }, 10000)
-    await availabilityOption.click()
+    const endDateToSelect = await this.driver.wait(until.elementLocated(By.xpath(`//td[@title='${rentalDetails.end_date}']`)), 10000)
+    await endDateToSelect.click() // Click on the desired date
 
-    /* ---------------------------------- cost---------------------------------- */
-    await this.driver.findElement(By.id('addNewItem_price_per_day')).sendKeys(itemDetails.price_per_day)
-    await this.driver.findElement(By.id('addNewItem_deposit')).sendKeys(itemDetails.deposit)
+    /* --------------------------------- prices --------------------------------- */
+    await this.driver.findElement(By.id('makeOffer_price_per_day')).sendKeys(rentalDetails.price_per_day)
+    await this.driver.findElement(By.id('makeOffer_deposit')).sendKeys(rentalDetails.deposit)
 
     /* --------------------------------- submit --------------------------------- */
-    const buttonElement = await this.driver.findElement(By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Add New Item']]"))
+    const buttonElement = await this.driver.findElement(
+      By.xpath("//button[contains(@class, 'ant-btn') and span[text()='Make Rental Offer']]")
+    )
     await this.driver.executeScript('arguments[0].scrollIntoView(true);', buttonElement) // Scroll to the button
     await this.driver.sleep(500)
     await buttonElement.click() // Click the button
--- tests/config.js
+++ tests/config.js
@@ -1,8 +1,15 @@
+const moment = require('moment')
+
 const config = {
   baseUrl: 'http://localhost:5173', // Replace with the actual URL of your homepage
   title: 'iRentStuff',
   username: 'test_user', // Replace with the actual username you expect to see
   password: 'test_user'
+}
+
+const secondaryUser = {
+  username: 'testuser', // Replace with the actual username you expect to see
+  password: 'testuser'
 }
 
 const newItemForm = {
@@ -25,4 +32,21 @@ const editItemForm = {
   deposit: 50
 }
 
-module.exports = { config, newItemForm, editItemForm }
+const newItemForOfferForm = {
+  title: 'test_offer',
+  category: 'Electronics',
+  condition: 'Excellent',
+  condition: 'Excellent',
+  availability: 'Available',
+  price_per_day: 10,
+  deposit: 50
+}
+
+const rentalOfferForm = {
+  start_date: moment().format('YYYY-MM-DD'),
+  end_date: moment().add(10, 'days').format('YYYY-MM-DD'),
+  price_per_day: parseFloat(10),
+  deposit: parseFloat(50)
+}
+
+module.exports = { config, secondaryUser, newItemForm, editItemForm, newItemForOfferForm, rentalOfferForm }
